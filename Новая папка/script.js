69702f5383c550ad33a82e1fc60907f5e8720068
dima <tolmd@ukr.net>
2021-07-24 22:21:15 +0300

    restore git

--- Новая папка/script.js
+++
@@ -1,6 +0,0 @@
-let number = 156798;
-let string = "IT School 'Hillel'";
-let boolean = false;
-console.log(`Value: ${number}; type: ${typeof(number)}`);
-console.log(`Value: ${string}; type: ${typeof(string)}`);
-console.log(`Value: ${boolean}; type: ${typeof(boolean)}`);
