b38c59b268dc9643d1c174736a2b979366a8ab8f
dima <tolmd@ukr.net>
2021-07-27 00:47:29 +0300

    add 4 task

--- homework_16/homework_16_4.js
+++ homework_16/homework_16_4.js
@@ -1,3 +1,12 @@
+/*
+4)* Напишите функцию, которая принимает аргумент number, создает матрицу в виде таблицы следующего типа:
+первая ячейка в первой строке содержит значение «1», а каждая следующая ячейка должна быть больше на «1»;
+первая ячейка второй строки содержит значение «2» и так далее...
+Напишите фукнцию, которая меняет цвет ячейкам в таблице, расположенным на обратной
+диагонали матрицы.
+*/
+
+let number = prompt('Enter number of strins in tabel')
 function makeMatrixFunction(number) {
     let table = document.querySelector('#matrix');
     for (let i = 1; i <= number; i++) {
@@ -6,13 +15,22 @@ function makeMatrixFunction(number) {
         for (let j = 0; j < number; j++) {
             let tdElements = document.createElement('td');
             tdElements.append(document.createTextNode(j + i));
-            if (i / j + 1 == 1) {
-                tdElements.classList.add('background-color')
-            }
             trElements.append(tdElements);
         };
         table.append(trElements);
     };
 };
 
-makeMatrixFunction(5);
+function changeDiagonalColor() {
+    let table = document.querySelector('#matrix');
+    let rows = table.rows;
+    for (let i = rows.length - 1; i >= 0; i--) {
+        for (let j = 0; j < rows.length; j++) {
+            if (i + j == rows.length - 1) {
+                rows[i].cells[j].style.background = 'red';
+            };
+        };
+    };
+};
+makeMatrixFunction(number);
+changeDiagonalColor();
