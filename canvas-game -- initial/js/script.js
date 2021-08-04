ea1d88669d0200aecea3f3f8ea7ae7adbaee5beb
dima <tolmd@ukr.net>
2021-08-04 19:22:03 +0300

    update script.js

--- canvas-game -- initial/js/script.js
+++ canvas-game -- initial/js/script.js
@@ -1,94 +1,95 @@
-const canvas = document.querySelector('#canvas');
-const startButton = document.querySelector('.start');
-const stopButton = document.querySelector('.stop');
-
-const ctx = canvas.getContext('2d');
-var dy = getRandomSpeed();
-let rectSize = 20;
-let rectColor = RandColor();
+const canvas = document.querySelector("#canvas");
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+const scoreId = document.getElementById('score');
+const ctx = canvas.getContext("2d");
 let x = getRandomInt(0, 640);
 let y = 0;
+let score = 0;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function RandColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 function getRandomSpeed() {
-	return Math.random() * 0.1;
-}
-function RandColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+  return Math.random() * 1.5;
 };
-// 
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function drawScore() {
+  document.getElementById('score').textContent = `${score}`;
 };
 
 function animate() {
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    updateRect();
+    drawScore();
+  };
 
-	function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		updateRect();
-		y += dy;
-	}
+  function updateRect() {
+    rectArray.forEach((rect) => {
+      drawRect(rect.xPos, rect.yPos, rect.color, rect.height, rect.width);
+      rect.yPos += rect.speed;
+    });
+  };
 
-	let rectArray = [];
-	for (let i = 0; i < 2; ++i) {
-		let newRect = {
-			color: RandColor(),
-			xPos: getRandomInt(0, 640),
-			yPos: y,
-		}
-		rectArray.push(newRect);
-		console.log(rectArray);
-	}
-	function updateRect() {
-		rectArray.forEach(rect => {
-			drawRect(rect.xPos, rect.yPos, rect.color)
-		})
-	}
-	function drawRect(x, y, color) {
-		ctx.fillStyle = color;
-		ctx.beginPath();
-		ctx.fillRect(x, y, 20, 20);
-		ctx.fill();
-		ctx.closePath();
-	}
+  function drawRect(x, y, color, height, width) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.fillRect(x, y, height, width);
+    ctx.fill();
+    ctx.closePath();
+  };
+  requestAnimationFrame(draw);
+};
 
-	if (y + dy > canvas.height - rectSize) {
-		clearInterval(interval);
-	}
-	var interval = setInterval(updateRect, 10);
-	stopButton.addEventListener('click', e => {
-		clearInterval(interval);
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-	});
-	setInterval(draw, 10)
-	requestAnimationFrame(animate);
+startButton.addEventListener("click", () => {
+  var interval = setInterval(animate, 100);
+  stopButton.addEventListener("click", () => {
+    score = 0;
+    clearInterval(interval);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    document.location.reload();
+  });
+});
+
+canvas.addEventListener("click", (e) => {
+  rectArray.forEach((rect, index) => {
+    if (
+      e.offsetX > rect.xPos &&
+      e.offsetY > rect.yPos &&
+      e.offsetX < rect.xPos + rect.width &&
+      e.offsetY < rect.yPos + rect.height
+    ) {
+      rectArray.splice(index, 1);
+      score++;
+      console.log(score)
+    }
+  });
+});
+
+let rectArray = [];
+for (let i = 0; i < 30; ++i) {
+  let newRect = {
+    color: RandColor(),
+    xPos: getRandomInt(0, 640),
+    yPos: -30,
+    width: 20,
+    height: 20,
+    speed: getRandomSpeed(),
+  };
+  rectArray.push(newRect);
 }
 
 
-
-
-
-canvas.addEventListener("click", e => {
-	console.log(`X and Y: ${e.offsetX}, ${e.offsetY}. `);
-	if (e.offsetX === x && e.offsetY === y) {
-		console.log('get')
-	} else { console.log('near') };
-	// if (e.offsetX < 100 && e.offsetX > 50 && e.offsetY > 50 && e.offsetY < 100) {
-	// 	output.innerText += `Вы кликнули по красной фигуре!`;
-	// 	output.style.color = 'red';
-	// } else if (e.offsetX < 180 && e.offsetX > 100 && e.offsetY > 120 && e.offsetY < 170) {
-	// 	output.innerText += `Вы кликнули по зеленой фигуре!`;
-	// 	output.style.color = 'green';
-	// } else {
-	// 	output.style.color = 'black';
-	// }
-});
-startButton.addEventListener('click', e => {
-	animate();
-});
