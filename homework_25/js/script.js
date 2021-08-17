9736e75f9207180fc78e6c0c76161aa8e12ee97f
dima <tolmd@ukr.net>
2021-08-17 23:13:02 +0300

    change script.js in homework 25

--- homework_25/js/script.js
+++ homework_25/js/script.js
@@ -9,33 +9,42 @@ function makeCard(name, status, species, location, image) {
     newCard.querySelector('.card-info').querySelector('h1').textContent = name;
     newCard.querySelector('.status').querySelector('p').textContent = species + ' -- ' + status;
     if (status === 'Dead') {
-        newCard.querySelector('.live-status').style = 'background:red'
+        newCard.querySelector('.live-status').classList.add('dead');
     }
     newCard.querySelector('.content').querySelector('p').textContent = location;
-    card.remove();
 }
 function filter() {
-    filterForm.addEventListener('click', function (event) {
-        if (event.target.id === 'male' || event.target.id === 'female') {
-            let url = `https://rickandmortyapi.com/api/character/?gender=${event.target.id}`;
-            createList(url);
-        } else if (event.target.id === 'alive' || event.target.id === 'dead') {
-            let url = `https://rickandmortyapi.com/api/character/?status=${event.target.id}`;
-            createList(url);
-        }
-    })
+    filterForm.addEventListener("change", function (event) {
+        console.log(event.target.id)
+        let url = `https://rickandmortyapi.com/api/character/`;
+        if (event.target.id === "male" || event.target.id === "female") {
+            url = `?gender=${event.target.id}`;
+        } else if (event.target.id === "alive" || event.target.id === "dead") {
+            url = `?status=${event.target.id}`;
+        };
+        createList(url);
+        document.querySelectorAll('.card').forEach(el => el.remove());
+    });
+}
+function createListOfRandomCharacters() {
+    let dataArr = [];
+    for (let i = 0; i < 10; i++) {
+        dataArr.push(Math.floor(Math.random() * (671 - 1) + 1));
+    }
+    const promise = fetch(`https://rickandmortyapi.com/api/character/${dataArr}`)
+        .then(response => response.json())
+        .then((data) => {
+            for (let elem of data) { makeCard(elem.name, elem.status, elem.species, elem.location.name, elem.image) }
+        })
+    filter();
 }
 function createList(url) {
-    for (let i = 0; i < 10; i++) {
-        const promise = fetch(url)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                makeCard(data.results[i].name, data.results[i].status, data.results[i].species, data.results[i].location.name, data.results[i].image);
-            })
-            .catch((err) => { console.warn(err) })
-    };
+    const promise = fetch('https://rickandmortyapi.com/api/character' + url)
+        .then(response => response.json())
+        .then((data) => {
+            for (let elem of data.results) { makeCard(elem.name, elem.status, elem.species, elem.location.name, elem.image) }
+        })
+        .catch((err) => { console.warn(err) })
 }
+createListOfRandomCharacters();
 card.remove()
-filter();
