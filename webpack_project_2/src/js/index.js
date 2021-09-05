79228ceaf4d9e6d54c192317d765bce484ff1080
dima <tolmd@ukr.net>
2021-09-05 22:09:16 +0300

    final update webpack_project_2.0

--- webpack_project_2/src/js/index.js
+++ webpack_project_2/src/js/index.js
@@ -1,13 +1,14 @@
 import { students } from './students';
 
 class Student {
-  constructor({ name, surname, ratingPoint, schoolPoint, course, isSelfPayment = true }) {
+  constructor({ name, surname, ratingPoint, schoolPoint, course, isSelfPayment = true, totalPoint = 0 }) {
     this.name = name;
     this.surname = surname;
     this.ratingPoint = ratingPoint;
     this.schoolPoint = schoolPoint;
     this.course = course;
     this.isSelfPayment = isSelfPayment;
+    this.totalPoint = totalPoint;
     this.id = Student.createId();
   }
   static createId = (function () { return this.i++ }).bind({ i: 1 })
@@ -22,10 +23,10 @@ class University {
     this.studentsArray.push(new Student(enrollee));
     this.studentsArray.sort((a, b) => b.totalPoint - a.totalPoint);
     this.studentsArray = this.studentsArray.map((student, index) => {
-      if (index <= 4) {
-        student.isSelfPayment = false;
-      } else { student.isSelfPayment = true }
-      return student;
+      return {
+        ...student,
+        isSelfPayment: index > 4
+      }
     })
   }
   getStudents() {
@@ -41,6 +42,13 @@ class University {
 }
 let khpiUniversity = new University("NTU 'KhPI'")
 students.forEach(student => khpiUniversity.addStudent(student))
+khpiUniversity.addStudent({
+  name: 'dima',
+  surname: 'dimov',
+  ratingPoint: 1400,
+  schoolPoint: 1200,
+  course: 3,
+})
 console.log('National Technical University – «Kharkiv Polytechnic Institute» :', khpiUniversity)
 console.log('Get all students:', khpiUniversity.getStudents())
 console.log('Students on budget education:', khpiUniversity.getBudgetStudents())
